e43e309d5a54aedc7bd11aaaaa75d9a42db23cd9
Anton Sokol <anton.sokol@gmail.com>
2011-01-11 17:58:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmoteRor::Application.config.secret_token = 'c421c3fea275d25558ade626a2da3bc16d41f92302d99380d761966c99be783b8b18034d8d361d2aada76adcbb1fbf48e2f2473d9302700522d3bcbbb8fd5097'
