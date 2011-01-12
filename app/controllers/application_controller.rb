e3a239495aaf0a39b45fbea5c9803a2d37737516
Anton Sokol <anton.sokol@gmail.com>
2011-01-12 13:31:11 +0200

    faster geocoding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
+require 'geokit'
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :geocode_by_ip
+  
+  protected
+    def geocode_by_ip
+      if session[:geo_location].nil? || session[:ip_address] != request.ip
+        session[:geo_location] = GeoKit::Geocoders::MultiGeocoder.geocode(request.ip)
+        session[:ip_address] = request.ip
+      end
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
-require 'geokit'
 class HomeController < ApplicationController
   def index
-    @location = GeoKit::Geocoders::MultiGeocoder.geocode(request.ip)
   end
 end
