9207acab72a7808c39174f93033885d1d00414f7
Anton Sokol <anton.sokol@gmail.com>
2011-01-19 01:20:28 +0200

    crud

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,15 +2,28 @@ class SurveysController < ApplicationController
   before_filter :authenticate_user!, :except => [:public_scorecard]
   before_filter :find_scorecard_by_token_or_bounce, :only => [:public_scorecard]
   def index
-    
+    @surveys = current_user.surveys.all
   end
   
   def new
-    
+    @survey = Survey.new
   end
   
   def create
-    
+    @survey = Survey.new params[:survey] 
+    @survey.user = current_user
+    if @survey.save
+        flash[:notice] = 'Emote published'
+        redirect_to account_surveys_path
+    else
+        render :action => 'new'
+    end
+  end
+  
+  def destroy
+    current_user.surveys.find(params[:id]).delete
+    flash[:notice] = 'Emote deleted'
+    redirect_to :action => 'index'
   end
   
   def scorecard
