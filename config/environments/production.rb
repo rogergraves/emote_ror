9ca9fb6585583a2e8b363af64ffc2c9823e4dce9
duhast <shtepsel_duhast@mail.ru>
2011-01-22 03:59:43 +0200

    production mailer cfg fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ EmoteRor::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'emotethis.com' }
+  config.action_mailer.default_url_options = { :host => 'web.emotethis.com' }
   
   #ActionMailer::Base.smtp_settings = {
   #  :address              => "smtp.gmail.com",
