8a629d279601f5a4fae750b7d60cec9f543f9dd4
Anton Sokol <anton.sokol@gmail.com>
2011-01-23 19:43:32 +0200

    pp tested

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,7 +4,8 @@ class SubscriptionsController < ApplicationController
   
   
   def index
-    @subscriptions = current_user.subscriptions.all
+    @subscriptions = current_user.current_subscriptions
+    @payments = current_user.subscriptions.all(:order => 'id DESC')
   end
   
   def new
@@ -22,7 +23,7 @@ class SubscriptionsController < ApplicationController
         :ip                => request.remote_ip,
         :return_url        => paypal_success_account_subscriptions_url(:only_path => false),
         :cancel_return_url => paypal_cancel_account_subscriptions_url(:only_path => false),
-        :subtotal => price * 100, :allow_guest_checkout => true, :no_shipping => 1, :description => "#{subscription_obj[:name]} for #{currency}#{price} ",
+        :subtotal => price * 100, :allow_guest_checkout => true, :no_shipping => 1, :description => "#{subscription_obj[:name]} for #{Country.curr_code_to_symbol(currency)}#{price} ",
         :currency => currency
       )
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
@@ -37,10 +38,9 @@ class SubscriptionsController < ApplicationController
     #    :payer_id => params[:PayerID],
     #    :token    => params[:token]
     #)
-
     if details.success?
     #if purchase.success?
-      subscription = Subscription.new(:start_date => DateTime.now)
+      subscription = Subscription.new(:start_date => DateTime.now, :user_id => current_user.id)
       subscription.emote_amount = subscription_obj[:amount] if (details.params['order_total'].to_f == subscription_obj[:price].to_f)
       subscription.trial = false #Auto sets duration
       transaction = PaypalTransaction.new
@@ -48,24 +48,25 @@ class SubscriptionsController < ApplicationController
       transaction.subscription = subscription
       transaction.token = details.token
       transaction.date = Time.now
-      transaction.currency = details.params['currency']
+      transaction.currency = details.params['order_total_currency_id']
       transaction.total = details.params['order_total']
       transaction.customer_name = [details.params['first_name'], details.params['middle_name'], details.params['last_name']].compact.join(' ')
       transaction.customer_id = details.payer_id
       transaction.customer_address = [details.params['street1'], details.params['city_name'], details.params['postal_code'], details.params['payer_country']].compact.join(', ')
       transaction.customer_email = details.email
       transaction.customer_phone = details.params['phone']
-      transaction.description = details.params['order_description']
+      transaction.description = subscription_obj[:name]
+      transaction.product_code = subscription_obj[:prod_code]
       subscription.save # do we need to save it? -=- It should be saved by transaction.save but we need to be 100% sure
       transaction.save
       flash[:notice] = "Thank you!"
     end
-    redirect_to account_surveys_path
+    redirect_to account_subscriptions_path
   end
   
   def paypal_cancel
     flash[:notice] = "You haven't completed the payment."
-    redirect_to account_surveys_path
+    redirect_to account_subscriptions_path
   end
     
 end
