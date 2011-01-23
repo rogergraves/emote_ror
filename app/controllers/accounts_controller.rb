ac610f38aef46cd2f93fcbad857a22ae4b51fe5f
Anton Sokol <anton.sokol@gmail.com>
2011-01-23 20:32:51 +0200

    edit account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,10 +2,23 @@ class AccountsController < ApplicationController
   before_filter :authenticate_user!
   
   def edit
-    
+    @account = current_user
   end
   
   def update
+    if params[:account][:password].blank?
+      params[:account].delete(:password)
+      params[:account].delete(:password_confirmation)
+    end
+    require 'pp'
+    pp params[:account]
+    @account = current_user
+    if @account.update_attributes params[:account]
+       
+    else
+      
+    end
     
+    render :action => 'edit'
   end
 end
