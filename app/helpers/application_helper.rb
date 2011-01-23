e1b40768c3d0b46b951d9354a42096ccf64e4844
duhast <shtepsel_duhast@mail.ru>
2011-01-24 01:02:12 +0200

    Error displaying polished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def flash_alert
+    return '' if alert.nil? || alert.empty?
+
+    html = <<-HTML
+    <div id="error_explanation">
+      <ul class="errors_list"><li>#{alert}</li></ul>
+    </div>
+    HTML
+    html.html_safe
+  end
+
 end
