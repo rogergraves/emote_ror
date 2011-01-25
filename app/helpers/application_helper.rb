8b61a168202d1cce83094dd897b72db3ba721a41
duhast <shtepsel_duhast@mail.ru>
2011-01-25 03:27:40 +0200

    'Update Personal Information' layout remastered; 'View activities' updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def errors_for(ar_instance)
+    return '' unless ar_instance.errors.any?
+    messages = ar_instance.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div id="error_explanation">
+      <ul class="errors_list">#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
