03684ea3d133d96ca7320f0d224e642183c65135
duhast <shtepsel_duhast@mail.ru>
2011-02-01 22:54:49 +0200

    [#15] updates

--- public/javascripts/emote_control.js
+++ public/javascripts/emote_control.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
 function showLinks(scorecard_direct_link){
     var dlg = $('#links-dialog')
     dlg.find('#scorecard_direct_link').val(scorecard_direct_link);
-    dlg.find('#scorecard_embed_link').val('<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'+scorecard_direct_link+'" />');
+    dlg.find('#scorecard_embed_link').val('<a href="'+scorecard_direct_link+'" target="emote">Click here</a>');
+    dlg.find('#open-emote-link').attr('href', scorecard_direct_link);
     dlg.dialog('open');
     return false;
 }
