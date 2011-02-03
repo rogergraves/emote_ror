92daeee5b98987e3b9cad42d133c1a12b83b43fa
Anton Sokol <anton.sokol@gmail.com>
2011-02-03 16:42:23 +0200

    destroy replaced with is_deleted

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -12,12 +12,13 @@ class SurveyResultsController < ApplicationController
   
   def delete_response
     # /account/surveys/:survey_id/survey_results/delete_response
-    @result = @survey.survey_results.find(:first, :conditions => { :survey_result_id => params[:id] })
     respond_to do |format|
       format.xml do
-        if @result.destroy
-          
-        else
+        begin
+          @result = @survey.survey_results.find(:first, :conditions => { :survey_result_id => params[:id] })
+          @result.id_deleted = true
+          @result.save
+        rescue
           
         end
       end
