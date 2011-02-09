6d73432d6191f709c9fefb4370933be85a35c709
duhast <shtepsel_duhast@mail.ru>
2011-02-09 02:32:16 +0200

    admin layout, admin accounts index

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -1,2 +1,37 @@
 class Admin::AccountsController < Admin::BaseController
+
+  def index
+    @users = User.paginate(:page => params[:page]) #, :conditions => ["`users`.`email` LIKE ? or `surveys`.`code` LIKE ? or `surveys`.`project_name` LIKE ?", "%#{params[:filter]}%","%#{params[:filter]}%","%#{params[:filter]}%"], :include => [:user])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "User #{user.email} successfullu saved"
+      redirect_to admin_accounts_path
+    else
+      flash[:alert] = 'Error saving user'
+      render :new
+    end
+  end
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes params[:user]
+      flash[:notice] = "User #{user.email} successfullu saved"
+      redirect_to admin_accounts_path
+    else
+      flash[:alert] = 'Error saving user'
+      render :new
+    end
+  end
+
 end
