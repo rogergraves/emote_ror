252ce1cd0293efaa8b931bebd264cdf639dd89e2
duhast <shtepsel_duhast@mail.ru>
2011-03-05 23:51:11 +0200

    #53 done

--- public/javascripts/emote_control.js
+++ public/javascripts/emote_control.js
@@ -1,30 +1,26 @@
 $(document).ready(function() {
     var dlg = $('#links-dialog');
     dlg.removeAttr('style').dialog({
-        title: 'Link to e.mote&trade;',
         autoOpen: false,
         closeOnEscape: true,
         modal: true,
         width: 550,
         height: 400
     });
-    dlg.find('input[type=text]').click(function(){
+    dlg.find('textarea').click(function(){
         this.select();
-    });
-    dlg.find('#open-emote-link').click(function(){
-        dlg.dialog('close');
-        return true;
     });
 });
 
-function showLinks(emote_link){
+function showLinks(emote_name, emote_link){
     var dlg = $('#links-dialog');
     dlg.find('#scorecard_direct_link').val(emote_link);
     dlg.find('#scorecard_embed_link').val('<a href="'+emote_link+'" target="_blank">Click here</a>');
     dlg.find('.feedback-code').each(function(){
       img_path = $(this).parent().prev().find('img').attr('src');
-      $(this).val('<a href="'+emote_link+'" target="_blank"><img src="'+img_path+'"/></a>')
+      $(this).val('<a href="'+emote_link+'" target="_blank"><img border="0" src="'+img_path+'"/></a>')
     });
+    dlg.dialog({title: 'Embed links for "'+emote_name+'" e.mote&trade; survey'});
     dlg.dialog('open');
     return false;
 }
