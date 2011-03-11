b14469c5d319988bf1a5a0c576aecffa858a46cc
duhast <shtepsel_duhast@mail.ru>
2011-03-11 13:22:11 +0200

    #58 done

--- public/javascripts/emote_control.js
+++ public/javascripts/emote_control.js
@@ -9,13 +9,22 @@ $(document).ready(function() {
     });
 });
 
-function showLinks(emote_name, emote_link){
+function showLinks(emote_name, emote_link, emote_code){
+    var embed_js =
+        "<div id='emote-this-button'></div>"+
+        "<script type='text/javascript'>"+
+        "emote_uid = '"+emote_code+"';"+
+        "emote_link_content = \"%CONTENT%\";"+
+        "var jsHost = (('https:' == document.location.protocol) ? 'https://ssl.' : 'http://');"+
+        "document.write(unescape('%3Cscript src=\"' + jsHost + 'emotethis.com/js/emote.js\" type=\"text/javascript\"%3E%3C/script%3E'));"+
+        "</script>";
+
     var dlg = $('#links-dialog');
     dlg.find('#scorecard_direct_link').val(emote_link);
-    dlg.find('#scorecard_embed_link').val('<a href="'+emote_link+'" target="_blank">Click here</a>');
+    dlg.find('#scorecard_embed_link').val(embed_js.replace(/%CONTENT%/, 'Click here'));
     dlg.find('.feedback-code').each(function(){
       img_path = $(this).parent().prev().find('img').attr('src');
-      $(this).val('<a href="'+emote_link+'" target="_blank"><img border="0" src="'+img_path+'"/></a>')
+      $(this).val(embed_js.replace(/%CONTENT%/, "<img src='"+img_path+"' border='0'/>"))
     });
     dlg.dialog({title: 'Embed links for "'+emote_name+'" e.mote&trade; survey'});
     dlg.dialog('open');
