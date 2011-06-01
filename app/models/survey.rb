876d24a17efeaebec8cbbf1c53f1f19555a1c40a
Anton Sokol <anton.sokol@gmail.com>
2011-06-01 18:02:21 +0300

    http://www.prestonkincaid.com/projects/public/index.php/projects/110/tickets/128

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -100,12 +100,12 @@ class Survey < ActiveRecord::Base
     self.action_token = Digest::MD5.hexdigest("#{id}-==-#{code}-=-#{Time.now}")
   end
 
-protected
+#protected
 
   def generate_code!(field)
     i = 0; kukan = 'x'
     loop do
-      kukan = Zlib::crc32("#{self.user ? self.user.full_name : 'no_user'}-#{self.user_id}--#{self.id}-#{self.project_name}-=-[OMATORE]-=#{i}=-#{Time.now.to_i}").to_s(36).upcase
+      kukan = Zlib::crc32("#{self.user ? self.user.full_name : 'no_user'}-#{self.user_id}--#{self.id}-#{self.project_name}-=-[OMATORE]-=#{i}=-#{Time.now.to_i}--#{field}").to_s(36).upcase
       break kukan unless Survey.find(:first, :conditions => { field.to_sym => kukan })
       i+=1
     end
