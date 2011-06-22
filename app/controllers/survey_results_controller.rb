1d8deb48d7fd18d1df24886d428403bd7a5ff567
Anton Sokol <anton.sokol@gmail.com>
2011-06-22 17:50:16 +0300

    zz

--- app/controllers/survey_results_controller.rb
+++ app/controllers/survey_results_controller.rb
@@ -51,7 +51,7 @@ class SurveyResultsController < ApplicationController
   end
   protected
     def find_survey
-      if !params[:survey_id].blank?
+      if !params[:survey_id].blank? && !current_user.nil?
         @survey = current_user.surveys.find_by_id(params[:survey_id])
       else
         @survey = Survey.find_by_code(params[:survey])
