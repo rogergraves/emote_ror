f2ad97f460bdeda9eb5176f886c71720272edc45
duhast <shtepsel_duhast@mail.ru>
2011-08-11 23:35:28 +0300

    exceptions email update

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,5 +2,5 @@ unless Rails.env.development?
   EmoteRor::Application.config.middleware.use ExceptionNotifier,
             :email_prefix => "[eMote #{Rails.env}] ",
             :sender_address => %{"notifier" <error@emotethis.com>},
-            :exception_recipients => %w{a@rubyriders.com oleg@rubyriders.com}
+            :exception_recipients => %w{a@rubyriders.com oleg+Alarm@rubyriders.com}
 end
