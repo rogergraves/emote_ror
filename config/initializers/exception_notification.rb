9639433d44271eeeb4dfc2ae3bcb229490ad3d4b
Roger Graves <rogergraves@gmail.com>
2013-10-22 11:15:54 -0700

    Remove exception notifications

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
-unless Rails.env.development?
-  EmoteRor::Application.config.middleware.use ExceptionNotifier,
-            :email_prefix => "[eMote #{Rails.env}] ",
-            :sender_address => %{"notifier" <error@emotethis.com>},
-            :exception_recipients => %w{a@rubyriders.com oleg+Alarm@rubyriders.com}
-end
+#unless Rails.env.development?
+#  EmoteRor::Application.config.middleware.use ExceptionNotifier,
+#            :email_prefix => "[eMote #{Rails.env}] ",
+#            :sender_address => %{"notifier" <error@emotethis.com>},
+#            :exception_recipients => %w{a@rubyriders.com oleg+Alarm@rubyriders.com}
+#end
